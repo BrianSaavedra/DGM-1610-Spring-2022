421be12de3ee779ab641263b39fd492b65237a88
Brian <10971425@my.uvu.edu>
2022-02-20 20:59:55 -0700

    Balloon Pops after multiple clicks.

---
+++ Prototype 2 - Balloon Pop Game/Assets/Scripts/Arrays/Arrays.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    public string[] friendNames = {"Bob", "Jessica", "Jody", "Arthur", "Randall", "Todd", "Becky"};
+    //                             { 0, 1, 2 ,3 ,4 ,5 ,6}
+    public int[] numbers ={23, 46, 77,10, 42, 98,63,81};
+
+    
+   
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        Debug.Log("Former friend " + friendNames[0]);
+        Debug.Log(friendNames[3]+ " Says Hi as he walks to class");
+        friendNames[0] = "Veronica";
+         Debug.Log("New friend " + friendNames[0]);
+         */
+         
+         foreach(int numbers in numbers)
+         {
+             print(numbers);
+         }
+         
+
+         
+    
+    }
+
+    
+}
