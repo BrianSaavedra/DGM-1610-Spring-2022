293a7b9cd7fe9a1021fd514d4a6cbf6a3f150f99
Brian <10971425@my.uvu.edu>
2022-03-26 18:33:06 -0600

    Added attack for both enemy and player

--- ProtoType 3 - Rouge Like Game/Assets/Scripts/PlayerController.cs
+++ ProtoType 3 - Rouge Like Game/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header ("Player Health")]
+    public int curHP;
+    public int maxHP;
+    
+    
+    
+    
+    
+    [Header ("Player Movement")]
     public float moveSpeed= 5f; // Speed at which the player will move
     private Rigidbody2D rb; // Store the referenced 2D rigidbody
-    Vector2 movement; //Store the player x,y position for movement
+    private Vector2 movement; //Store the player x,y position for movement
+    private Vector2 direction; 
+   
+   [Header ("Player Combat")]
+
+   public float AttackRange;// Range at which the player can attack
+   public float attackRate;
+   private float lastAttackTime;
+   public int damage;
+   public LayerMask enemyLayer;
+   
     // Start is called before the first frame update
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -18,6 +38,14 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal"); // Input for Left Right Movement
         movement.y = Input.GetAxis("Vertical"); // Input for Up Down Movement
+
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            if(Time.time - lastAttackTime >= attackRate)
+                   Attack();
+
+
+        }
     }
 
     //Set number of calls per fram
@@ -26,5 +54,53 @@ public class PlayerController : MonoBehaviour
     {
         // Apply physics and moves the character 
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+        UpdateDirection();
+
     }
-}
+
+    void UpdateDirection()
+    {
+        Vector2 vel = new Vector2(movement.x,movement.y);
+
+        if(vel.magnitude !=0)
+        {
+            direction = vel;
+        }
+
+        rb.velocity = vel * moveSpeed;
+
+    }
+
+
+    void Attack()
+    {
+        lastAttackTime = Time.time;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, AttackRange, enemyLayer);
+
+        if(hit.collider != null)
+        {
+            hit.collider.GetComponent<Enemy>()?.TakeDamage(damage);
+      
+        }
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        curHP -= damage;
+
+        if(curHP <= 0)
+        {
+            Die();
+        }
+
+
+    void Die()
+    {
+        Debug.Log("Player has bit the dust!");
+    }
+    
+    }
+}   
