18620feafc577e0c3c124cbde95951c08d8a6d31
Brian <10971425@my.uvu.edu>
2022-04-19 15:52:16 -0600

    Added the loops

--- Retro Fps Clone/Assets/Scripts/Assignments/Loops.cs
+++ Retro Fps Clone/Assets/Scripts/Assignments/Loops.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour
 {
+
+
+
     /*
     public int numEwoks = 0;
     */
+    /*
     public int ducksInPond = 99;
+    */
+    /*
+    public int numEnemys = 21;
+    */
+    /*
+    public int EnemysInTheField = 100;
+    */
+    /*
+    public int numPeople = 40;
+    */
+    /*
+    public int innocentPeople = 100;
+    */
+    /*
+    public int numOfAllies = 52;
+    */
+    /*
+    public int DeadAllies = 42; 
+    */
+    /*
+    public int numCasualties = 100;
+    */
+    /*
+    public int TotalDamage = 10000;
+    */
+
+
+
+
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +53,81 @@ public class Loops : MonoBehaviour
         }
         */
 
+        /*
         while(ducksInPond > 0)
         {
             Debug.Log(ducksInPond);
             Debug.Log("All of the ducks have flown south for the winter");
             ducksInPond --;
         }
+        */
+
+         /*
+         for(int i = 0; i < numEnemys; i++)
+        {
+            Debug.Log("There are " + i + " Enemys on the far south side!");
+        }
+        */
+
+        /*
+        while(EnemysInTheField > 0)
+        {
+            Debug.Log(EnemysInTheField);
+            Debug.Log("There are hunderdes of enemys!");
+            EnemysInTheField --;
+        }
+        */
+        /* 
+         for(int i = 0; i < numPeople; i++)
+        {
+            Debug.Log( i + " People have been evacuated!");
+        }
+        */
+
+        /*
+        while(innocentPeople > 0)
+        {
+            Debug.Log(innocentPeople);
+            Debug.Log("All of the people who were in the cross fire have evacuated!");
+            innocentPeople --;
+        }
+        */
+        /*
+        for(int i = 52; i > numOfAllies; i--)
+        {
+            Debug.Log("We still out number them by " + i + "!" );
+        }
+        */
+        
+
+        /*
+        while(DeadAllies > 0)
+        {
+            Debug.Log(DeadAllies);
+            Debug.Log("100 of us have been killed ");
+            DeadAllies --;
+        }
+        */
+        /*
+        for(int i = 100; i > numCasualties; i--)
+        {
+            Debug.Log("There were" + i + "Casualties!" );
+        }
+        */
+        
+
+        /*
+        while(TotalDamage > 0)
+        {
+            Debug.Log(TotalDamage);
+            Debug.Log("We at least did 10000 damage to them!");
+            TotalDamage --;
+        }
+        */
+
+
+
+
     }
 
     // Update is called once per frame
@@ -31,4 +135,5 @@ public class Loops : MonoBehaviour
     {
         
     }
+    
 }
